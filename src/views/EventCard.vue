0c2ac28978335932c8aaa47e9e06f997c6ab96f2
Bison Hubert <bisonhubert@gmail.com>
2020-01-28 15:55:30 -0800

    Add and style event card

--- src/views/EventCard.vue
+++ src/views/EventCard.vue
@@ -1,16 +1,21 @@
 <template>
-  <div>
-    <h4>{{ name }}</h4>
-    <router-link :to="url">Click to view details</router-link>
-  </div>
+  <router-link :to="url" class="event-link">
+    <div class="event-card -shadow">
+      <span class="eyebrow">Starts at {{ event.checkIn }} on {{ event.startDate }}</span>
+      <h4 class="title">{{ event.name }}</h4>
+      <span>{{ event.attendees.length }} attending</span><br>
+      <span>Ends {{ event.endDate }} at {{ event.checkOut }}</span>
+      
+    </div>
+  </router-link>
 </template>
 
 <script>
 export default {
   name: "EventCard",
   props: {
-    name: {
-      type: String,
+    event: {
+      type: Object,
       required: true
     },
     url: {
@@ -18,8 +23,27 @@ export default {
       required: true
     }
   }
-}
+};
 </script>
 
 <style scoped>
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
+}
 </style>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="event-list">
     <h1>All Events</h1>
-      <EventCard v-for="event in eventsList" :key="event.id" :name="event.name" :url="{ name: 'event-show', params: { id: event.id }}" />
+    <EventCard
+      v-for="event in eventsList"
+      :key="event.id"
+      :event="event"
+      :url="{ name: 'event-show', params: { id: event.id } }"
+    />
   </div>
 </template>
 
 <script>
-import EventCard from "@/views/EventCard"
+import EventCard from "@/views/EventCard";
 
 export default {
   name: "EventList",
@@ -15,11 +20,26 @@ export default {
   },
   data() {
     return {
-      eventsList: [{ id: 1, name: "Bachelor Party" }]
+      eventsList: [
+        {
+          id: 1,
+          name: "Bachelor Party",
+          startDate: "Friday May 29, 2020",
+          endDate: "Sunday May 31, 2020",
+          checkIn: "3:00 PM",
+          checkOut: "12:00PM",
+          attendees: [
+            { id: 1, name: 'Tomas Rodriguez' },
+            { id: 2, name: 'James Bolotin' },
+            { id: 3, name: 'Joe Ott' },
+            { id: 4, name: 'Bryce Yockey' },
+            { id: 5, name: 'Grant Sloan' }
+          ]
+        }
+      ]
     };
   }
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
