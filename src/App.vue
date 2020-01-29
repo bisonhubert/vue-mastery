fb69cb8255888ac2896d614fe18ff530909e03ba
Bison Hubert <bisonhubert@gmail.com>
2020-01-28 16:02:47 -0800

    Add and style nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link :to="{ name: 'event-list' }">List</router-link> |
-      <router-link :to="{ name: 'event-create' }">Create Event</router-link>
-    </div>
+    <NavBar />
     <router-view />
   </div>
 </template>
+
+<script>
+import NavBar from "@/views/NavBar"
+
+export default {
+  name: "App",
+  components: {
+    NavBar
+  }
+}
+</script>
 
 <style>
 html {
