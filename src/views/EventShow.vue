dcc5418ff24924795d5eedfa1da17208a868e9a8
Bison Hubert <bisonhubert@gmail.com>
2020-01-28 17:29:49 -0800

    Style event show page

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,24 +1,43 @@
 <template>
-  <div class="event-show">
-    <h1>Event {{ id }} Details</h1>
-    <div>{{ description }}</div>
-  </div>
-</template>
+      <div>
+        <div class="event-header">
+          <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+          <h1 class="title">{{ event.title }}</h1>
+          <h5>Organized by {{ event.organizer }}</h5>
+          <h5>Category: {{ event.category }}</h5>
+        </div>
+        <BaseIcon name="map"><h2>Location</h2></BaseIcon>
+        <address>{{ event.location }}</address>
+        <h2>Event details</h2>
+        <p>{{ event.description }}</p>
+        <h2>Attendees
+          <span class="badge -fill-gradient">{{ attendeeCount }}</span>
+        </h2>
+        <ul class="list-group">
+          <li v-for="(attendee, index) in event.attendees" :key="index" class="list-item">
+            <b>{{ attendee.name }}</b>
+          </li>
+        </ul>
+      </div>
+    </template>
 
 <script>
-import EventService from "@/services/EventService";
+import EventService from "@/services/EventService"
 
 export default {
   name: "EventShow",
   props: ["id"],
   data() {
     return {
-      event: null
-    };
+      event: {}
+    }
   },
   computed: {
-    description() {
-      return this.event ? this.event.description : null;
+    attendees() {
+      return this.event.attendees || []
+    },
+    attendeeCount() {
+      return this.attendees.length 
     }
   },
   created() {
@@ -32,3 +51,24 @@ export default {
   }
 };
 </script>
+    
+<style scoped>
+.location {
+  margin-bottom: 0;
+}
+.location > .icon {
+  margin-left: 10px;
+}
+.event-header > .title {
+  margin: 0;
+}
+.list-group {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.list-group > .list-item {
+  padding: 1em 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+</style>
