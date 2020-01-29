d2832d74a890576c74cd339b25e6bf7287ba4209
Bison Hubert <bisonhubert@gmail.com>
2020-01-28 17:25:32 -0800

    Add event details

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="event-show">
     <h1>Event {{ id }} Details</h1>
+    <div>{{ description }}</div>
   </div>
 </template>
 
 <script>
+import EventService from "@/services/EventService";
+
 export default {
   name: "EventShow",
-  props: ["id"]
+  props: ["id"],
+  data() {
+    return {
+      event: null
+    };
+  },
+  computed: {
+    description() {
+      return this.event ? this.event.description : null;
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(response => {
+        this.event = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 };
 </script>
