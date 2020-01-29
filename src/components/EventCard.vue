39af4423f30d2450ec7388e5d916ceb36f78bd7d
Bison Hubert <bisonhubert@gmail.com>
2020-01-28 17:13:46 -0800

    Prettier formatting

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -5,8 +5,7 @@
         >Starts at {{ event.time }} on {{ event.date }}</span
       >
       <h4 class="title">{{ event.title }}</h4>
-      <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon
-      >
+      <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon>
     </div>
   </router-link>
 </template>
