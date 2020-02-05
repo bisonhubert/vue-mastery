55f1a1790e1afcde5524dc41ee40e96984c6791e
Bison Hubert <bisonhubert@gmail.com>
2020-02-04 16:14:34 -0800

    Add create event form

--- 02-mastering-vuex/src/views/EventCreate.vue
+++ 02-mastering-vuex/src/views/EventCreate.vue
@@ -1,3 +1,80 @@
 <template>
-  <h1>Create Event</h1>
+  <div class="event-create">
+    <h1>Create Event</h1>
+    <form>
+      <label>Select a category</label>
+      <select v-model="event.category">
+        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+      </select>
+      <h3>Name & describe your event</h3>
+      <div class="field">
+        <label>Title</label>
+        <input v-model="event.title" type="text" placeholder="Add an event title"/>
+      </div>
+      <div class="field">
+        <label>Description</label>
+        <input v-model="event.description" type="text" placeholder="Add a description"/>
+      </div>
+      <h3>Where is your event?</h3>
+      <div class="field">
+        <label>Location</label>
+        <input v-model="event.location" type="text" placeholder="Add a location"/>
+      </div>
+      <h3>When is your event?</h3>
+      <div class="field">
+        <label>Date</label>
+        <datepicker v-model="event.date" placeholder="Select a date"/>
+      </div>
+      <div class="field">
+        <label>Select a time</label>
+        <select v-model="event.time">
+          <option v-for="time in times" :key="time">{{ time }}</option>
+        </select>
+      </div>
+      <input type="submit" class="button -fill-gradient" value="Submit"/>
+    </form>
+  </div>
 </template>
+
+<script>
+import Datepicker from 'vuejs-datepicker'
+
+export default {
+  name: 'EventCreate',
+  components: {
+    Datepicker
+  },
+  data() {
+    const times = [];
+    for(var i=1;i<=24;i++) {
+      times.push(i)
+    }
+    return {
+      times,
+      event: this.createFreshEvent(),
+      categories: this.$store.state.categories
+    }
+  },
+  methods: {
+    createFreshEvent() {
+      const user = this.$store.state.user
+      return {
+        category: null,
+        organizer: user,
+        title: null,
+        description: null,
+        location: null,
+        date: null,
+        time: null,
+        attendees: []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.field {
+  margin-bottom: 24px
+}
+</style>
