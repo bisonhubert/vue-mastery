d5cd50b8905b9dfe1698544db21b32cdd9410f39
Bison Hubert <bisonhubert@gmail.com>
2020-02-06 16:41:34 -0800

    Add next and prev buttons

--- 02-mastering-vuex/src/views/EventList.vue
+++ 02-mastering-vuex/src/views/EventList.vue
@@ -2,6 +2,12 @@
   <div>
     <h1>Events Listing</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <template v-if="!isFirstPage">
+      <router-link :to="{ name: 'event-list', query: { page: prevPageNum }}" rel="prev">Prev Page</router-link>
+    </template>
+    <template v-if="!isLastPage">
+      <router-link :to="{ name: 'event-list', query: { page: nextPageNum }}" rel="next">Next Page</router-link>
+    </template>
   </div>
 </template>
 
@@ -15,14 +21,40 @@ export default {
   components: {
     EventCard
   },
+  data() {
+    return {
+      perPage: 3
+    }
+  },
   computed: {
     ...mapState(["events"]),
     page() {
       return this.$route.query.page || 1;
+    },
+    isFirstPage() {
+      return this.page === 1
+    },
+    isLastPage() {
+      return this.page < (this.events / this.perPage)
+    },
+    prevPageNum() {
+      return this.isFirstPage
+        ? 1
+        : this.page - 1
+    },
+    nextPageNum() {
+      return this.isLastPage
+        ? this.page
+        : this.page + 1
     }
   },
   created() {
-    this.$store.dispatch("getEvents", { perPage: 3, page: this.page });
+    this.$store.dispatch("getEvents", { perPage: this.perPage, page: this.page });
+  },
+  watch: {
+    page(nextPage) {
+      this.$store.dispatch("getEvents", { perPage: this.perPage, page: nextPage });
+    }
   }
 };
 </script>
