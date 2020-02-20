182cecad3c64522134fe6e8bf199454edb504c18
Bison Hubert <bisonhubert@gmail.com>
2020-02-19 17:02:40 -0800

    Display notifications

---
+++ 02-mastering-vuex/src/components/NotificationBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="notification-bar" :class="typeClass">
+    <p>{{ notification.message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotificationBar",
+  props: {
+    notification: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    typeClass() {
+      return `-text-${this.notification.type}`;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.notification-bar {
+  margin: 1em 0 1em;
+}
+.text-error {
+  color: red;
+}
+.text-success {
+  color: green;
+}
+</style>
